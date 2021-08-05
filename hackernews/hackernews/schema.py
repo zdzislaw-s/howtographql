082f8461f1a58cdefff9e41fb59e7defe88fbab0
Zdzislaw Sliwinski <z.sliwinski@abysssolutions.com.au>
2021-08-05 16:49:08 +1000

    Create User

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -1,12 +1,13 @@
 import graphene
 
 import links.schema
+import users.schema
 
 
 class Query(links.schema.Query, graphene.ObjectType):
         pass
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
