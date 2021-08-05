dafefb7b53e9d879a7bd8edd02347beb8aeac829
Zdzislaw Sliwinski <z.sliwinski@abysssolutions.com.au>
2021-08-06 09:53:30 +1000

    Test Authentication

--- hackernews/users/schema.py
+++ hackernews/users/schema.py
@@ -11,9 +11,16 @@ class UserType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
+    me = graphene.Field(UserType)
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Not logged in!")
+        return user
 
 
 class CreateUser(graphene.Mutation):
